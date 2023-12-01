106d7ca15107bf0c146d9b04b937158009bfc664
alexpar2 <alex.prza@gmail.com>
2023-12-01 18:44:59 +0100

    primer commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menuToggle').addEventListener('click', function() {
+  var menuContainer = document.querySelector('.box.titular');
+  menuContainer.classList.toggle('active');
+});
